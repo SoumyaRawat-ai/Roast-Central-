dcd68a5742a1f5a6e5875646c95fd263d7c11c89
Soumya Rawat <soumyarawat0305@gmail.com>
2025-04-20 07:13:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/components/icons.ts:38:9
    > Parsing ecmascript source code failed
    >   36 |
    >   37 |       <svg
    > > 38 |         xmlns="http://www.w3.org/2000/svg"
    >      |         ^^^^^
    >   39 |         width="24"
    >   40 |         height="24"
    >   41 |         viewBox="0 0 24 24"
    >
    > Expected '>', got 'xmlns'
    >
    >

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -1,4 +1,4 @@
-import {ArrowRight, Check, ChevronsUpDown, Circle, Copy, Edit, ExternalLink, File, HelpCircle, Home, Loader2, Mail, MessageSquare, Moon, Plus, PlusCircle, Search, Server, Settings, Share2, Shield, Sun, Trash, User, X, Workflow} from 'lucide-react';
+import {ArrowRight, Check, ChevronsUpDown, Circle, Copy, Edit, ExternalLink, File, HelpCircle, Home, Loader2, Mail, MessageSquare, Moon, Plus, PlusCircle, Search, Server, Settings, Share2, Shield, Sun, Trash, User, X, Workflow, Fire} from 'lucide-react';
 
 const icons = {
   arrowRight: ArrowRight,
@@ -33,26 +33,26 @@ const icons = {
 
 function FireIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M14.5 9.5 10 14.5" />
-      <path d="M14.5 14.5 10 9.5" />
-      <path d="M4 18h1.4c.7 0 1.3.5 1.4 1.2l.1.8H18v-1.1c0-.7-.5-1.3-1.2-1.4l-.8-.1H6v1.1c0 .7.5 1.3 1.2 1.4l.8.1H4z" />
-      <path d="M15 12c0-1.6 1.3-3 3-3s3 1.4 3 3c0 1.4-.9 2.5-2.1 2.9a.5.5 0 0 1-.2.1.5.5 0 0 1-.3-.1C15.9 14.5 15 13.4 15 12z" />
-    </svg>
+    
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+      >
+        <path d="M14.5 9.5 10 14.5" />
+        <path d="M14.5 14.5 10 9.5" />
+        <path d="M4 18h1.4c.7 0 1.3.5 1.4 1.2l.1.8H18v-1.1c0-.7-.5-1.3-1.2-1.4l-.8-.1H6v1.1c0 .7.5 1.3 1.2 1.4l.8.1H4z" />
+        <path d="M15 12c0-1.6 1.3-3 3-3s3 1.4 3 3c0 1.4-.9 2.5-2.1 2.9a.5.5 0 0 1-.2.1.5.5 0 0 1-.3-.1C15.9 14.5 15 13.4 15 12z" />
+      </svg>
+    
   );
 }
 
 export {icons};
-
-    
